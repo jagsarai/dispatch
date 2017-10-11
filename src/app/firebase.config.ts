2694a1afe72310ffe51d1e257a2df33eebfea8d5
jagsarai <sarai.jagvir@gmail.com>
2017-10-10 17:55:36 -0700

    upload modal working with firebase

---
+++ src/app/firebase.config.ts
@@ -0,0 +1,8 @@
+export const FIREBASE_CONFIG = {
+    apiKey: "AIzaSyAPccgfBHvfSxluR4QRWER2Ukiu8C4KC4Y",
+    authDomain: "simpledispatch-a02d7.firebaseapp.com",
+    databaseURL: "https://simpledispatch-a02d7.firebaseio.com",
+    projectId: "simpledispatch-a02d7",
+    storageBucket: "simpledispatch-a02d7.appspot.com",
+    messagingSenderId: "254875405163"
+}
--- src/pages/upload-modal/upload-modal.ts
+++ src/pages/upload-modal/upload-modal.ts
@@ -4,6 +4,8 @@ import { File } from '@ionic-native/file';
 import { Transfer, TransferObject } from '@ionic-native/transfer';
 import { FilePath } from '@ionic-native/file-path';
 import { Camera } from '@ionic-native/camera';
+import { storage, initializeApp} from 'firebase';
+import { FIREBASE_CONFIG } from '../../app/firebase.config';
 
 declare var cordova: any;
 
@@ -21,7 +23,7 @@ export class UploadModalPage {
   duplicateImage:Boolean = false;
 
   constructor(public navCtrl: NavController, private camera: Camera, private transfer: Transfer, private file: File, private filePath: FilePath, public actionSheetCtrl: ActionSheetController, public toastCtrl: ToastController, public platform: Platform, public loadingCtrl: LoadingController, public viewCtrl: ViewController) { 
-
+    initializeApp(FIREBASE_CONFIG);
   }
   
 
@@ -60,12 +62,15 @@ export class UploadModalPage {
         quality: 100,
         sourceType: sourceType,
         saveToPhotoAlbum: false,
-        correctOrientation: true
+        correctOrientation: true,
+        encodingType: this.camera.EncodingType.JPEG,
+        mediaType: this.camera.MediaType.PICTURE
       };
 
       // Get the data of an image
-      this.camera.getPicture(options).then((imagePath) => {
+       this.camera.getPicture(options).then((imagePath) => {
         // Special handling for Android library
+        console.log("This is the proper image path " + imagePath);
         if (this.platform.is('android') && sourceType === this.camera.PictureSourceType.PHOTOLIBRARY) {
           this.filePath.resolveNativePath(imagePath)
             .then(filePath => {
@@ -83,29 +88,46 @@ export class UploadModalPage {
       });  
    }
 
+  // public async takePicture(sourceType){
+  //   try{
+  //     var options = {
+  //       quality: 100,
+  //       sourceType: sourceType,
+  //       saveToPhotoAlbum: false,
+  //       correctOrientation: true,
+  //       destinationType: this.camera.DestinationType.DATA_URL,
+  //       encodingType: this.camera.EncodingType.JPEG,
+  //       mediaType: this.camera.MediaType.PICTURE
+  //     };
+
+  //     const result = await this.camera.getPicture(options);
+
+  //     const pictures = storage().ref('/loads/');
+  //     const image = `data:image/jpeg;base64,${result}`;
+
+  //     pictures.putString(image, 'data_url')
+  //   }
+  //   catch(e){
+  //     console.error(e);
+  //   }
+  // }
+
   // Create a new name for the image
   private createFileName() {
     var d = new Date(),
-    n = d.getTime(),
+    n = d.getHours() + '' + d.getMinutes() + '' + d.getSeconds(),
     newFileName =  n + ".jpg";
     return newFileName;
   }
-
-  //check for duplicate image
-  // private checkImagesForDuplicate(imgName){
-  //   this.images.map((imageName) => {
-  //     if(imageName === imgName){
-  //       return this.duplicateImage = true;
-  //     }
-  //   });
-  //   return this.duplicateImage = false;
-  // }
  
   // Copy the image to a local folder
   private copyFileToLocalDir(namePath, currentName, newFileName) {
     
     this.file.copyFile(namePath, currentName, cordova.file.dataDirectory, newFileName).then(success => {
+      console.log("Copy event fired");
+      console.log("Success storing in local folder " +  cordova.file.dataDirectory + " " + newFileName);
       this.images.push(newFileName);
+      this.lastImage  = this.images[this.images.length - 1];
     }, error => {
       this.presentToast('Error while storing file.');
     });
@@ -131,7 +153,7 @@ export class UploadModalPage {
 
   // public uploadImage() {
   //   // Destination URL
-  //   var url = "http://yoururl/upload.php";
+  //   var url = "gs://simpledispatch-a02d7.appspot.com/";
    
   //   // File for Upload
   //   var targetPath = this.pathForImage(this.lastImage);
@@ -156,19 +178,45 @@ export class UploadModalPage {
    
   //   // Use the FileTransfer to upload the image
   //   fileTransfer.upload(targetPath, url, options).then(data => {
-  //     this.loading.dismissAll()
-  //     this.presentToast('Image succesful uploaded.');
+  //     this.loading.dismiss()
+  //     this.presentToast('Image succesfully uploaded.');
   //   }, err => {
-  //     this.loading.dismissAll()
+  //     this.loading.dismiss()
   //     this.presentToast('Error while uploading file.');
   //   });
   // }
 
-  public uploadImage(){
-    //Distination Url 
-    var url = ''
-  }
+  public async uploadImage(){
+    try{
+      console.log("This is the image name " + this.lastImage);
+      this.loading = this.loadingCtrl.create({
+        content: 'Uploading...',
+       });
 
+      this.loading.present();
+
+      const image = await this.file.readAsDataURL(cordova.file.dataDirectory, this.lastImage);
+
+      console.log("This is the file " + image);
+  
+      const storageRef = storage().ref('/loads/' + this.lastImage);
+      
+      storageRef.putString(image, 'data_url');
+
+
+      this.images = [];
+      this.loading.dismiss();
+      this.presentToast("Upload successfull");
+    }
+   
+    catch(e){
+      this.loading.dismiss();
+      this.presentToast("Upload failed, please try again");
+      console.error(e);
+    }
+        
+  }
+  
   closeUploadModal(){
     this.viewCtrl.dismiss();
   }
